f9b9b45e2eeaceffbef87b9661363f72f5a64680
harukaR <awwrate1218@gmail.com>
2023-04-23 23:13:27 +0900

    必要なコンポーネントを追加

--- src/components/UserIcon/UserIcon.tsx
+++ src/components/UserIcon/UserIcon.tsx
@@ -1,11 +1,14 @@
 import styles  from "@/components/UserIcon/userIcon.module.scss"
+type Props ={
+    children:string;
+}
 
-export const UserIcon =()=>{
+export const UserIcon =({children}:Props)=>{
     return(
 
         <div className={styles.UserWrap}>
                 <div className={styles.userIcon}></div>
-                <p className={styles.userName}>こんにちはUser01さん</p>
+                <p className={styles.userName}>{children}</p>
         </div>
     )
 }
