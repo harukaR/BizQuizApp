fc76c9c912637a5353508ea4bd91ec7a980c4817
harukaR <awwrate1218@gmail.com>
2023-05-02 23:02:52 +0900

    footerコンポーネントを追加した

---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+// import { faMoon } from '@fortawesome/free-solid-svg-icon'
+
+// import styles from "@/components/Layout/Footer/footer.module.scss"
+
+
+
+export const Footer =()=>{
+    return(
+        <footer>
+           <p>©︎2023 @haruka_o</p>
+        </footer>
+
+    )
+}
