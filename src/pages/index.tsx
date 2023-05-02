600532ac93a6f841b5b90d571a33d06b67f7d2a4
harukaR <awwrate1218@gmail.com>
2023-05-02 21:10:34 +0900

    デザインを変更するので一旦削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,23 +15,6 @@ export default function Home() {
       <main>  
         <SiteTitle/>
         <div className={styles.mainInner}>
-        <UserIcon children='こんにちはuser01さん'/>
-            <div className={styles.buttonWrap}>
-              <Link href="/quiz" className={styles.link}>
-                <SelectButton children="クイズに挑戦する" className={styles.challengeButton} />
-              </Link>
-
-              <div className={styles.buttonFlex}>
-              <Link href="/qanda" className={styles.link}>
-                <SelectButton children="問題と回答をみる"/>
-              </Link>
-              <Link href="/score" className={styles.link}>
-                <SelectButton children="本日のあなたのスコア"/>
-              </Link>
-
-
-              </div>
-            </div>
         </div>
       </main>
     </>
